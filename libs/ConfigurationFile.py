1c89671e92fcd177e9609042f22f955f3bf7e8ee
IlyaVishnikin <rarebloor@yandex.ru>
2022-12-26 02:07:35 +0300

    Added master-key hashing

--- libs/ConfigurationFile.py
+++ libs/ConfigurationFile.py
@@ -1,5 +1,7 @@
 import json
 
+from argon2 import PasswordHasher
+
 class ConfigurationFile:
 	"""
 	ConfigurationFile - class that representates configuration file that Gpam using.
@@ -40,7 +42,7 @@ class ConfigurationFile:
 
 		self.data['configuration']['vaults'].append({
 			"names": [vault_name],
-			"master-key": master_key,
+			"master-key": PasswordHasher().hash(master_key) if master_key else "",
 			"path": vault_path
 		})
 		self.set_default_vault(vault_name)
@@ -53,7 +55,7 @@ class ConfigurationFile:
 	def set_vault_master_key(self, vault_name: str, master_key: str) -> None:
 		for vault in self.data['configuration']['vaults']:
 			if vault_name in vault['names']:
-				vault['master-key'] = master_key
+				vault['master-key'] = PasswordHasher().hash(master_key) if master_key else ""
 
 	def set_default_vault(self, vault_name: str) -> None:
 		for vault in self.data['configuration']['vaults']:
