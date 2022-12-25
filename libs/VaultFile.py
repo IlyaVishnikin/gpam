d9a60875152bec97aac1d58383d8c5b9572dafec
IlyaVishnikin <rarebloor@yandex.ru>
2022-12-26 02:20:25 +0300

    Added add_fields method

--- libs/VaultFile.py
+++ libs/VaultFile.py
@@ -14,12 +14,27 @@ class VaultFile:
 
 		except (json.JSONDecodeError, FileNotFoundError): # recreating configuration file
 			vault_file = {
-				"vault": {}
+				"vault": []
 			}
 			with open(self.path, "w+") as f:
 				json.dump(vault_file, f, indent=4)
 			with open(self.path, "r") as f:
 				self.data = json.load(f)
+
+	def add_fields(self, **kwargs) -> None:
+		if "login" not in kwargs:
+			return
+
+		# checks that record with the same login already exists.
+		# If record with the same login exists then update fields
+		for record in self.data['vault']:
+			if record['login'] == kwargs['login'] and record['site'] == kwargs['site'] and record['password'] == kwargs['password']:
+				# change already existed fields
+				for k, v in kwargs:
+					record[k] = v
+				return
+
+		self.data['vault'].append(kwargs)
 
 	def save(self):
 		with open(self.path, "w") as f:
