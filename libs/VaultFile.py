2a2fe812d3ff644f3d0e0e79ce114b02f5542a9d
IlyaVishnikin <rarebloor@yandex.ru>
2022-12-26 22:48:23 +0300

    Added functional from crypto and ConfigurationFile

--- libs/VaultFile.py
+++ libs/VaultFile.py
@@ -1,5 +1,14 @@
 import json
 
+from base64 import b64encode, b64decode
+from argon2 import PasswordHasher
+
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
 class VaultFile:
 	"""
 	VaultFile - class that representates vault file
@@ -14,6 +23,7 @@ class VaultFile:
 
 		except (json.JSONDecodeError, FileNotFoundError): # recreating configuration file
 			vault_file = {
+				"master-key": "",
 				"vault": []
 			}
 			with open(self.path, "w+") as f:
@@ -22,19 +32,35 @@ class VaultFile:
 				self.data = json.load(f)
 
 	def add_fields(self, **kwargs) -> None:
-		if "login" not in kwargs:
+		if "login" not in kwargs or "site" not in kwargs:
 			return
 
-		# checks that record with the same login already exists.
-		# If record with the same login exists then update fields
+		if "password" in kwargs and self.data['master-key']:
+			# encrypt password with master key
+			key = PBKDF2(kwargs["password"], b"{self.data['master-key']}", dkLen=32, count=10**6)
+			cipher = AES.new(key, AES.MODE_CBC)
+			ciphered_data = cipher.encrypt(pad(b"{kwargs['password']}", AES.block_size))
+			kwargs["password"] = b64encode(ciphered_data).decode()
+			kwargs["aes_iv"] = str(cipher.iv)
+
 		for record in self.data['vault']:
-			if record['login'] == kwargs['login'] and record['site'] == kwargs['site'] and record['password'] == kwargs['password']:
-				# change already existed fields
+			if record['login'] == kwargs['login'] and record['site'] == kwargs['site']:
 				for k, v in kwargs:
-					record[k] = v
+					if k not in record:
+						record[k] = v
 				return
 
 		self.data['vault'].append(kwargs)
+
+	def set_master_key(self, master_key: str) -> None:
+		if not master_key:
+			return
+
+		self.data['master-key'] = PasswordHasher().hash(master_key)
+
+	def get_master_key(self) -> str:
+		return self.data['master-key']
+
 
 	def save(self):
 		with open(self.path, "w") as f:
