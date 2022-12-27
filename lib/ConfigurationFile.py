6fc889a1878bd33ed0497db7e0d39b5e33fb6e7a
IlyaVishnikin <rarebloor@yandex.ru>
2022-12-27 21:02:12 +0300

    Added special instance variables

--- lib/ConfigurationFile.py
+++ lib/ConfigurationFile.py
@@ -18,6 +18,9 @@ class ConfigurationFile:
 			}
 			self.save()
 
+		self.vaults = self.data["config"]["vaults"]
+		self.default_vault = self.data["config"]["default-vault"]
+
 	def save(self) -> None:
 		with open(self.path, "w") as json_file:
 			json.dump(self.data, json_file, indent=4)
