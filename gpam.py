9200a26a3a04d85741e7716feec223d972142f70
IlyaVishnikin <rarebloor@yandex.ru>
2022-12-27 20:18:44 +0300

    Added a general view for the 'new' command

---
+++ gpam.py
@@ -0,0 +1,20 @@
+import click
+
+
+@click.group()
+def gpam(): ...
+
+
+@gpam.command()
+@click.option("--vault", "-v", type=str, help="Vault name(if not set than uses default vault)")
+@click.option("--login", "-l", type=str, help="New login name")
+@click.option("--site", "-s", type=str, help="New site")
+@click.option("--password", "-p", type=str, help="New password")
+@click.option("--field", "-f", type=(str, str), multiple=True, help="Custome field")
+@click.option("--master-key", "-m", type=str, help="Master key")
+@click.option("--interacitve", "-i", is_flag=True, help="Interactive input of fields")
+def new(vault, login, site, password, field):
+	...
+
+if __name__ == "__main__":
+	gpam()
