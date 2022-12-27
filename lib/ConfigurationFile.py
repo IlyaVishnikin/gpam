ab293987dcd6a1a8d8d0ef395fe12d8603c28830
IlyaVishnikin <rarebloor@yandex.ru>
2022-12-27 20:58:47 +0300

    Added save method

--- lib/ConfigurationFile.py
+++ lib/ConfigurationFile.py
@@ -6,11 +6,9 @@ class ConfigurationFile:
 
 	def __init__(self, path: str):
 		self.path = path
-
 		try:
 			with open(self.path, "r") as json_file:
 				self.data = json.load(json_file)
-
 		except:
 			self.data = {
 				"config": {
@@ -18,5 +16,8 @@ class ConfigurationFile:
 					"vaults": []
 				}
 			}
-			with open(self.path, "a+") as json_file:
-				json.dump(self.data, json_file, indent=4)
+			self.save()
+
+	def save(self) -> None:
+		with open(self.path, "w") as json_file:
+			json.dump(self.data, json_file, indent=4)
