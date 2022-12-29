d9b61bbf15545ddbcf30f2c44a462537ef7021bb
IlyaVishnikin <rarebloor@gmail.com>
2022-12-29 09:32:54 +0300

    Added 'add_record' method

--- lib/VaultFile.py
+++ lib/VaultFile.py
@@ -2,6 +2,10 @@ import json
 
 from base64 import b64encode
 from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 
@@ -20,13 +24,35 @@ class VaultFile:
 		except FileNotFoundError:
 			self.data = {
 				"salt": b64encode(get_random_bytes(128)).decode(),
-				"master-key": "" if not master_key else PasswordHasher().hash(master_key),
+				"master-key": "" if not master_key else PasswordHasher().hash(self.master_key),
 				"records": []
 			}
 			self.save()
 		except (json.decoder.JSONDecodeError, VerifyMismatchError):
 			self.data = {}
 
+	def add_record(self, **fields):
+		if not fields:
+			raise ValueError("Record fields is not specified")
+
+		if "password" in fields:
+			key = PBKDF2(self.master_key, self.data["salt"].encode('ascii'), dkLen=32, count=10**6)
+			aes = AES.new(key, AES.MODE_CBC)
+			ciphered_password = aes.encrypt(pad(fields["password"].encode('ascii'), AES.block_size))
+			aes_iv = aes.iv
+			fields["password"] = b64encode(key + aes_iv + ciphered_password).decode()
+
+		record_root = None
+		for record in self.data["records"]:
+			if record["site"] == fields["site"] and record["login"]:
+				record_root = record
+		if not record_root:
+			self.data["records"].append({})
+			record_root = self.data["records"][-1]
+
+		for field_name in fields:
+			if field_name not in record_root:
+				record_root[field_name] = fields[field_name]
 
 	def save(self):
 		with open(self.path, "w") as json_file:
