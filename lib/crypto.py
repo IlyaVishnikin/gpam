5f7c9dc87f8348469ab7de9ef7f98c4c645e5150
IlyaVishnikin <rarebloor@gmail.com>
2022-12-31 13:38:27 +0300

    Remove functionality to gpam.py

--- lib/crypto.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-def generate_password(length: int) -> str:
-	if length < 0:
-		raise ValueError(f"Invalid length: {length}")
-
-	alphabet = string.ascii_letters + string.digits + string.punctuation
-	return ''.join(random.choices(alphabet, k=length))
